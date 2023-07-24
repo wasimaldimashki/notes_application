eb59acce5b87592bff68be89299c4c8389a65845
unknown <aldemashky2001@gmail.com>
2023-07-24 22:38:39 +0300

    Generate a note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,16 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteModel(
@@ -10,3 +19,5 @@ class NoteModel {
       required this.date,
       required this.color});
 }
+// Generate a note model
+// flutter packages pub run build_runner build
